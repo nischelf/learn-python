70991878125931693c9216bc95c1d3aa06c370c9
nischelf <nischelwitzer.fabian@a1.net>
2024-12-21 21:28:53 +0100

    convert csv data to json

--- csv-to-json/main.py
+++ csv-to-json/main.py
@@ -2,23 +2,34 @@ import sys
 import csv
 
 args = sys.argv
-print(args)
+# print(args)
 delimiter = ";"
 quotechar = "'"
 
 if "--delimiter" in args:
     delimiter = args[args.index("--delimiter") + 1]
-    print(delimiter)
+    # print(delimiter)
 
 if "--quotechar" in args:
     quotechar = args[args.index("--quotechar") + 1]
-    print(quotechar)
+    # print(quotechar)
 
 file = args[-1]
 
 fileContext = open(file, "r", newline="")
 # print(fileContext.read())
 
+json = []
+properties = []
+line = 1
 csvContext = csv.reader(fileContext, delimiter=delimiter, quotechar=quotechar)
 for row in csvContext:
-    print(row)
+    if line == 1:
+        properties = row
+    else:
+        row_dict = {properties[i]: row[i] for i in range(len(properties))}
+        json.append(row_dict)
+
+    line += 1
+
+print(json)
